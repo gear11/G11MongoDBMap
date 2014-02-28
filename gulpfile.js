051089ddc661006998ba9bdbe25f842f42da742f
Andy Jenkins <andrewbjenkins@gmail.com>
2014-02-27 22:57:52 -0500

    Add image files.

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// See: http://travismaynard.com/writing/getting-started-with-gulp
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserify = require('gulp-browserify');
+
+var paths = {
+  scripts: ['web/js/*.js', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true']
+};
+
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src(paths.scripts[0])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts[0])
+        .pipe(browserify({ debug: true }))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('g11-maps.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('web/js/*.js', ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch']);
